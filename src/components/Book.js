d341050cc47b90a8f529da49e8221517d7828d59
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-05-10 06:58:53 +0300

    Start Bootstrap template used for CSS Styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import './Book.css';
 
 class Book extends React.Component {
   render() {
     return (
-      <div>
-        <h2>{this.props.book.author}</h2>
-        <h4>{this.props.book.title}</h4>
+      <div class="col-lg-4 col-sm-6 mb-4">
+        <div class="portfolio-item">
+          <a
+            class="portfolio-link"
+            data-bs-toggle="modal"
+            href="#portfolioModal1"
+          >
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src={this.props.book.imageURL} alt="..." />
+          </a>
+          <div class="portfolio-caption">
+            <div class="portfolio-caption-heading">
+              {this.props.book.author}
+            </div>
+            <div class="portfolio-caption-subheading text-muted">
+              {this.props.book.title}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
