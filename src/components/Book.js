35bf48054ec3c1dcc915fd1a6c4bca5f81aedbe7
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-05-10 07:01:38 +0300

    some class name changes by jsx classname format

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,25 +4,29 @@ import './Book.css';
 class Book extends React.Component {
   render() {
     return (
-      <div class="col-lg-4 col-sm-6 mb-4">
-        <div class="portfolio-item">
+      <div className="col-lg-4 col-sm-6 mb-4">
+        <div className="portfolio-item">
           <a
-            class="portfolio-link"
+            className="portfolio-link"
             data-bs-toggle="modal"
             href="#portfolioModal1"
           >
-            <div class="portfolio-hover">
-              <div class="portfolio-hover-content">
-                <i class="fas fa-plus fa-3x"></i>
+            <div className="portfolio-hover">
+              <div className="portfolio-hover-content">
+                <i className="fas fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src={this.props.book.imageURL} alt="..." />
+            <img
+              className="img-fluid"
+              src={this.props.book.imageURL}
+              alt="..."
+            />
           </a>
-          <div class="portfolio-caption">
-            <div class="portfolio-caption-heading">
+          <div className="portfolio-caption">
+            <div className="portfolio-caption-heading">
               {this.props.book.author}
             </div>
-            <div class="portfolio-caption-subheading text-muted">
+            <div className="portfolio-caption-subheading text-muted">
               {this.props.book.title}
             </div>
           </div>
