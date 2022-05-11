c47fe9f04ca01cb87063bc748f6aed756905a30c
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-05-11 17:33:22 +0300

    Provider and Consumer method / Context Api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Booklist from './Booklist';
+import BookContextProvider from '../contexts/BookContext';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Booklist />
+        <BookContextProvider>
+          <Booklist />
+        </BookContextProvider>
       </div>
     );
   }
